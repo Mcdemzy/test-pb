eec6a0eb77c6ad32015e982f494f6485f35d15f7
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-02-27 19:44:56 +0100

    `Update text size in Signup page: changed font size from 0.875rem to 0.75rem`

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -34,7 +34,7 @@ const Signup = () => {
           <h2 className="md:text-[2rem] text-[1.5rem] font-semibold text-gray-900 text-center">
             Sign up for an account
           </h2>
-          <p className="mb-6 text-[.875rem] md:text-[1rem] text-[#667085] text-center font-normal leading-6">
+          <p className="mb-6 text-[.75rem] md:text-[1rem] text-[#667085] text-center font-normal leading-6">
             Get started today by entering just a few details
           </p>
           <form className="space-y-6">
